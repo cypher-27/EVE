45390d4c9a4a0fd4eab631bff053f12357632796
Cypher <cyphernexon27@gmail.com>
2026-03-02 00:02:55 -0600

    ostype

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,7 @@ resource "proxmox_lxc" "entorno_lxc" {
   }
   
   # Plantilla base (Asegúrate de haberla descargado en Proxmox)
+  ostype       = each.value.os == "alpine" ? "alpine" : "debian"
   ostemplate   = each.value.os == "alpine" ? "local:vztmpl/alpine-3.23-default_20260116_amd64.tar.xz" : "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
   unprivileged = true
   
